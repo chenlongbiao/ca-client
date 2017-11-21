0fedc2cc107269efded8332642ad7f5f5f54d1a3
陈龙飚 <714147655@qq.com>
2017-11-21 12:37:30 +0800

    首页优化

--- config/index.js
+++ config/index.js
@@ -8,8 +8,8 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    devtool: 'cheap-module-source-map',
-    productionSourceMap: true,
+    // devtool: 'cheap-module-source-map',
+    productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
