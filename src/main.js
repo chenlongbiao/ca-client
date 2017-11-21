8c59a2430205d43d81a1fba15c190b0fc6110bb1
陈龙飚 <714147655@qq.com>
2017-11-21 15:50:17 +0800

    打包优化

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {Input, Button, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem} from 'element-ui'
+import {Input, Button, Menu, Submenu, MenuItem, Breadcrumb, Pagination, BreadcrumbItem} from 'element-ui'
 // import 'element-ui/lib/theme-default/index.css'
 import 'element-ui/lib/theme-default/button.css'
-import 'iview/dist/styles/iview.css'
 import { store } from './store/store'
 import 'vue-event-calendar/dist/style.css'
 import vueEventCalendar from 'vue-event-calendar'
@@ -18,7 +17,7 @@ Vue.component(Menu.name, Menu)
 Vue.component(MenuItem.name, MenuItem)
 Vue.component(Breadcrumb.name, Breadcrumb)
 Vue.component(BreadcrumbItem.name, BreadcrumbItem)
-// Vue.component(Pagination.name, Pagination)
+Vue.component(Pagination.name, Pagination)
 // Vue.component(MenuItemGroup.name, MenuItemGroup)
 Vue.component(Submenu.name, Submenu)
 // Vue.use(Button)
